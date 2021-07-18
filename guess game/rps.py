f5c235d18f0bffe8e6a2a613519f7c44a6583a2f
brownhilludeh <85250041+brownhilludeh@users.noreply.github.com>
2021-07-18 13:20:14 +0100

    Add files via upload

---
+++ guess game/rps.py
@@ -0,0 +1,48 @@
+import random
+
+
+# def rps():
+#     user = input(
+#         "choice? \n'r' for rock. \n's' for scissor \n'p' for paper \n")
+#     comp = random.choice(['r', 'p', 's'])
+
+#     if user == comp:
+#         return "it's a tie"
+
+#     if win(user, comp):
+#         return (f'You won!. Comp chose {comp}')
+
+#     return (f'You lost. Comp chose {comp}')
+
+
+# def win(player, opponent):
+#     if (player == 'r' and opponent == 's') \
+#             or (player == 's' and opponent == 'p') \
+#             or (player == 'p' and opponent == 'r'):
+#         return True
+
+#     # return play()
+# print(rps())
+
+def rps():
+    user = input(
+        "choice? \n'r' for rock. \n's' for scissor \n'p' for paper \n")
+    comp = random.choice(['r', 'p', 's'])
+
+    if user == comp:
+        return "it's a tie"
+
+    if win(user, comp):
+        return (f'You won!. Comp chose {comp}')
+    
+    return (f'You lost. Comp chose {comp}')
+
+
+def win(player, opponent):
+    if (player == 'r' and opponent == 's') \
+            or (player == 's' and opponent == 'p') \
+            or (player == 'p' and opponent == 'r'):
+        return True
+
+    # return play()
+print(rps())
