ed802a27cd0afaea95ed6bee6a7d4cb2857e02cb
Brownhill Udeh <85250041+brownhilludeh@users.noreply.github.com>
2021-07-24 23:07:14 +0100

    Update rps.py

--- guess game/rps.py
+++ guess game/rps.py
@@ -1,28 +1,6 @@
 import random
 
 # feel free to edit this code, make modifications and share
-# def rps():
-#     user = input(
-#         "choice? \n'r' for rock. \n's' for scissor \n'p' for paper \n")
-#     comp = random.choice(['r', 'p', 's'])
-
-#     if user == comp:
-#         return "it's a tie"
-
-#     if win(user, comp):
-#         return (f'You won!. Comp chose {comp}')
-
-#     return (f'You lost. Comp chose {comp}')
-
-
-# def win(player, opponent):
-#     if (player == 'r' and opponent == 's') \
-#             or (player == 's' and opponent == 'p') \
-#             or (player == 'p' and opponent == 'r'):
-#         return True
-
-#     # return play()
-# print(rps())
 
 def rps():
     user = input(
